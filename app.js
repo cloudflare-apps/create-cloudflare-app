12e97c9100d1602046a0f43661afa9a5163726cd
Connor Peshek <connorpeshek@gmail.com>
2016-06-09 19:05:40 -0400

    Flesh out app.
    - Add JS that makes a div with hello text.
    - Fill out json with hello text.
    - Add info in readme explaining how to use this project.
    - Add styling to CSS to example how to use it.

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
   if (!window.addEventListener) return // Check for IE9+
 
   let options = INSTALL_OPTIONS
+  let element
 
   function updateElement() {
+    element = Eager.createElement(options.location, element)
+
+    const welcome = document.createElement("eager-welcome")
+
+    welcome.innerText = options.welcomeText
+    welcome.className = "eager-welcome"
+    element.appendChild(welcome)
   }
 
   if (document.readyState === "loading") {
